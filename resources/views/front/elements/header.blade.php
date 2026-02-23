5f0f8836df6a8a99ee7c174972632aefb1c7b996
Hxtreme <rwpttech@gmail.com>
2026-02-23 13:38:45 +0530

    social-media-added

--- resources/views/front/elements/header.blade.php
+++ resources/views/front/elements/header.blade.php
@@ -55,8 +55,15 @@ $get_agri_categories = Category::get_categories('agri-products');
                         </ul>
                      </li>
                      <li><a href="{{ route('contactus') }}">Contact Us</a></li>
-                     <li><a rel="nofollow" target="_blank" href="https://www.instagram.com/navyug.namdhari?igsh=MWZseWRqOW54dXBwYg=="><i class="fa-brands fa-instagram"></i></a></li>
-                     <li><a rel="nofollow" target="_blank" href="https://www.linkedin.com/company/navyug-group-nng/"><i class="fa-brands fa-linkedin"></i></a></li>
+                     
+					 <li>
+                        <a href="javacript:;"> Social Links</a>
+                        <ul>
+                            <li><a rel="nofollow" target="_blank" href="https://www.instagram.com/navyug.namdhari?igsh=MWZseWRqOW54dXBwYg=="><i class="fa-brands fa-instagram"></i></a></li>
+                            <li><a rel="nofollow" target="_blank" href="https://www.linkedin.com/company/navyug-group-nng/"><i class="fa-brands fa-linkedin"></i></a></li>
+                        </ul>
+                     </li>
+				
                   </ul>
                </div>
             </div>
